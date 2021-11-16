f10d6c025c20af069c3076765c59775788853584
Arthur Rocha Lima <2001arthur.rocha@gmail.com>
2021-11-16 08:04:37 -0300

    TopicoController e ExceptionHandler

--- src/main/java/br/com/alura/forum/repository/TopicoRepository.java
+++ src/main/java/br/com/alura/forum/repository/TopicoRepository.java
@@ -4,6 +4,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import br.com.alura.forum.modelo.Topico;
 
+import java.util.List;
+
 public interface TopicoRepository extends JpaRepository<Topico, Long>{
 
+    List<Topico> findByCursoNome(String nomeCurso);
 }
